7cd85c1068d9c4b525ddd7e08fcbebb6b249e8e2
ekan07 <aniekanbon@gmail.com>
2022-09-15 01:48:17 +0100

    Update files: setup_mysql_dev.sql setup_mysql_test.sql

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,10 +1,8 @@
--- prepares a MySQL server for the AirBnB clone
--- create a database
+-- Prepares a MySQL server for the project.
+-- create project developement database with the name : hbnb_dev_db
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
--- create a user and grant privileges
-GRANT ALL ON hbnb_dev_db.*
-TO 'hbnb_dev'@'localhost'
-IDENTIFIED BY 'hbnb_dev_pwd';
--- grant SELECT privileges on performance_schema
-GRANT SELECT ON performance_schema.*
-TO 'hbnb_dev'@'localhost';
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+GRANT ALL PRIVILEGES ON hbnb_dev_db . * TO 'hbnb_dev'@'localhost';
+FLUSH PRIVILEGES;
+GRANT SELECT ON performance_schema . * TO 'hbnb_dev'@'localhost';
+FLUSH PRIVILEGES;
--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,10 +1,8 @@
--- prepares a MySQL test server for the AirBnb clone
--- creates a database
+-- Prepares a MySQL Test server for the project.
+-- create project testing database with the name : hbnb_test_db
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
--- create a user and grant privileges
-GRANT ALL ON hbnb_test_db.*
-TO 'hbnb_test'@'localhost'
-IDENTIFIED BY 'hbnb_test_pwd';
--- grant SELECT privileges
-GRANT SELECT ON performance_schema.*
-TO 'hbnb_test'@'localhost';
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+GRANT ALL PRIVILEGES ON hbnb_test_db . * TO 'hbnb_test'@'localhost';
+FLUSH PRIVILEGES;
+GRANT SELECT ON performance_schema . * TO 'hbnb_test'@'localhost';
+FLUSH PRIVILEGES;
