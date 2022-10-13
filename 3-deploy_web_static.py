9a774f6beb2f01724d50bd7293c4ae9db1d974ee
ekan07 <aniekanbon@gmail.com>
2022-10-13 21:11:12 +0100

    3-deploy_web_static.py

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -71,7 +71,7 @@ def do_deploy(archive_path):
 def deploy():
     """Create and distributes an archive to your web servers"""
     archive_path = do_pack()
-    if archive_path is None:
+    if not archive_path:
         return False
 
     return do_deploy(archive_path)
